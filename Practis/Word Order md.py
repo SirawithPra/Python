e6139850f6e2293f7e311f999238d86a9cebe034
SirawithPra <sirawith.pra@gmail.com>
2022-12-19 15:33:43 +0700

    Word Order (hackerrank)
    Create

---
+++ Practis/Word Order md.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+num_elements = int(input())
+elements = []
+counts = {}
+
+for i in range(num_elements):
+    element = input()
+    elements.append(element)
+
+counts = Counter(elements)
+print(len(set(elements)))
+
+for element, count in counts.items():
+    print(count, end=' ')
